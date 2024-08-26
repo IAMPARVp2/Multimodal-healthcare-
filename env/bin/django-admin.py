af55383e174f6de12112430051906715773568c0
parv jain <jainv1071@gmail.com>
2024-08-26 15:17:08 +0530

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alice/code/Heart-and-Kidney-disease-prediction-Django-master/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ env/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.3")
+SET(OpenBLAS_INCLUDE_DIRS /home/alice/code/Heart-and-Kidney-disease-prediction-Django-master/env/include)
+SET(OpenBLAS_LIBRARIES /home/alice/code/Heart-and-Kidney-disease-prediction-Django-master/env/lib/libopenblas.so)
